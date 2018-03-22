7ceb514af78ec245af85b02721672722364be04b
Legoburner73 <legoplaysmcmail@gmail.com>
2018-03-22 19:47:06 +0000

    Connection to server for uploadGJAccComment.php
    :)

--- incl/comments/uploadGJAccComment.php
+++ incl/comments/uploadGJAccComment.php
@@ -25,6 +25,22 @@ if($id != "" AND $comment != "" AND $GJPCheck->check($gjp,$id) == 1){
 	$query = $db->prepare("INSERT INTO acccomments (userName, comment, userID, timeStamp)
 										VALUES (:userName, :comment, :userID, :uploadDate)");
 	$query->execute([':userName' => $userName, ':comment' => $comment, ':userID' => $userID, ':uploadDate' => $uploadDate]);
+	$inside_data = array('username' => $userName, 'message' => $decodecomment, 'userID' => $userID, 'uploadDate' => $uploadDate);
+        $inside_data_string = json_encode($inside_data);
+        $data = array("event" => "acccomment_upload", "data" => $inside_data_string);                                                                    
+	$data_string = json_encode($data);                                                                                   
+                                                                                                                     
+	$ch = curl_init('http://legodev.glitch.me/api/dragongdps');                                                                      
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    		'Content-Type: application/json',                                                                                
+    		'Content-Length: ' . strlen($data_string))                                                                       
+	);                                                                                                                   
+                                                                                                                     
+	$result = curl_exec($ch);
+	curl_close($ch);
 	echo 1;
 }else{
 	echo -1;
