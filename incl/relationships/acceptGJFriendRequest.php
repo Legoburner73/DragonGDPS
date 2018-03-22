51f6fb337c5e13778b351b947c32bdbf0b3f84f6
Legoburner73 <legoplaysmcmail@gmail.com>
2018-03-22 19:55:00 +0000

    now we know when you get a friend :)
    wooo!

--- incl/relationships/acceptGJFriendRequest.php
+++ incl/relationships/acceptGJFriendRequest.php
@@ -29,6 +29,23 @@ if($gjpresult == 1){
 	//REMOVING THE REQUEST
 	$query = $db->prepare("DELETE from friendreqs WHERE ID=:requestID LIMIT 1");
 	$query->execute([':requestID' => $requestID]);
+	//WHY ARE WE SHOUTING?
+	$inside_data = array('user' => $reqAccountID, 'friend' => $toAcccountID);
+        $inside_data_string = json_encode($inside_data);
+        $data = array("event" => "friend_made", "data" => $inside_data_string);                                                                    
+	$data_string = json_encode($data);                                                                                   
+                                                                                                                     
+	$ch = curl_init('http://legodev.glitch.me/api/dragongdps');                                                                      
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    		'Content-Type: application/json',                                                                                
+    		'Content-Length: ' . strlen($data_string))                                                                       
+	);                                                                                                                   
+                                                                                                                     
+	$result = curl_exec($ch);
+	curl_close($ch);
 	//RESPONSE SO IT DOESNT SAY "FAILED"
 	echo "1";
 }else{
